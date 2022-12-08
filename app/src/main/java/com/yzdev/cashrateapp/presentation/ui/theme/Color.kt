59d63932b6966d6f96198f8367b487c68c2d5db7
jesus medina <jm9417174@gmail.com>
2022-12-08 15:52:41 -0400

    [UPD] Color theme

--- app/src/main/java/com/yzdev/cashrateapp/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/yzdev/cashrateapp/presentation/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+/** colores custom*/
+val BlackMain = Color(0xFF1E1E1E)
+val WhiteMain = Color(0xFFF5F5F5)
+val Orange = Color(0xFFFF9800)
--- app/src/main/java/com/yzdev/cashrateapp/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/yzdev/cashrateapp/presentation/ui/theme/Theme.kt
@@ -5,17 +5,19 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = WhiteMain,
+    primaryVariant = Color.White,
+    secondary = Orange
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = BlackMain,
+    primaryVariant = Color.Black,
+    secondary = Orange,
+    background = WhiteMain
 
     /* Other default colors to override
     background = Color.White,
